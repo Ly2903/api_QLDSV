b9f6a4d7fdb2a63a11d04e49a973d015847b5b77
Ly2903 <tttrucly2903@gmail.com>
2023-05-14 15:17:59 +0700

    update

--- routes/taiKhoanRoute.js
+++ routes/taiKhoanRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { TaoTaiKhoan } from "../controller/TaiKhoanController.js";
+import {
+  TaoTaiKhoan,
+  getDSTaiKhoan,
+} from "../controller/TaiKhoanController.js";
 
 const router = express.Router();
 
 //--------auth
 router.post("/taoTaiKhoan", TaoTaiKhoan);
+router.post("/getDSTaiKhoan", getDSTaiKhoan);
 
 export default router;
